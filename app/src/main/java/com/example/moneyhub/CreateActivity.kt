88c480e09c59fdfb4a9fbb73658987e80ce7e4ae
OhHuijin <145532762+OhHuijin@users.noreply.github.com>
2024-11-08 01:08:51 +0900

    버튼 연결

--- app/src/main/java/com/example/moneyhub/CreateActivity.kt
+++ app/src/main/java/com/example/moneyhub/CreateActivity.kt
@@ -1,33 +1,20 @@
 package com.example.moneyhub
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        // login.xml 레이아웃을 설정
-        setContentView(R.layout.login)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+        setContentView(R.layout.create_room)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-
             insets
-
-        }
-        val button_open_create:ConstraintLayout = findViewById(R.id.button_open_create)
-        button_open_create.setOnClickListener {
-            val intent = Intent(this, CreateActivity::class.java)
-            startActivity(intent)
-
         }
     }
 }
