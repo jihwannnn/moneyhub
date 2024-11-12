7418d3ecebb5c4783a1d84ab971a274988cd0504
OhHuijin <145532762+OhHuijin@users.noreply.github.com>
2024-11-12 23:59:15 +0900

    색 변경 및 여러가지 바꿈

--- app/src/main/java/com/example/moneyhub/CreateActivity.kt
+++ app/src/main/java/com/example/moneyhub/CreateActivity.kt
@@ -10,7 +10,7 @@ class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.create_room)
+        setContentView(R.layout.activity_create)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
