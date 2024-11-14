e083fa2c4b483de47a5573db9feea62aac744974
Hwang Seongmin <hsmin040326@gmail.com>
2024-11-15 03:47:16 +0900

    navigation 편집중

--- app/src/main/java/com/example/moneyhub/MainActivity.kt
+++ app/src/main/java/com/example/moneyhub/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.moneyhub
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.moneyhub.databinding.ActivityMainPageBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainPage : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainPageBinding
 
@@ -23,10 +22,10 @@ class MainPage : AppCompatActivity() {
         binding = ActivityMainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Applying WindowInsets
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // custom_header_include에만 WindowInsets 적용
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.custom_header_include)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
@@ -51,6 +50,11 @@ class MainPage : AppCompatActivity() {
                     if (destination.id == R.id.MembersFragment) R.drawable.icon_members_on else R.drawable.icon_members_off
                 )
             }
+        }
+
+        binding.customHeaderInclude.imageViewMyPage.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/moneyhub/MyPageActivity.kt
+++ app/src/main/java/com/example/moneyhub/MyPageActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MyPage : AppCompatActivity() {
+class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
